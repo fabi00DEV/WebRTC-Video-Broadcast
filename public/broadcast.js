993e70498c14ba962d8aad2bde07353c12a6f3ae
fabio <fabi00.dev@gmail.com>
2020-06-02 19:02:44 -0300

    add turns

--- public/broadcast.js
+++ public/broadcast.js
@@ -1,9 +1,29 @@
 const peerConnections = {};
 const config = {
   iceServers: [
-    {
-      urls: ["stun:stun.l.google.com:19302"]
-    }
+      { url: 'stun:18.220.202.114:5349' },
+      { url: 'stun:stun.l.google.com:19302' },
+      { url: 'stun:stun1.l.google.com:19302' },
+      { url: 'stun:stun2.l.google.com:19302' },
+      { url: 'stun:stun3.l.google.com:19302' },
+      { url: 'stun:stun4.l.google.com:19302' },
+      { url: 'stun:stun.services.mozilla.com' },
+      { url: 'stun:meet-jit-si-turnrelay.jitsi.net:443' },
+      {
+        url: 'turn:18.220.202.114:5349',
+        username: 'smash',
+        credential: 'hc4qjVYChz3kBRhN',
+      },
+      { 
+        url: 'turns:18.220.202.114:443?transport=tcp',
+        username: 'meet.smash.cx',
+        credential: '60xNNKDk1zLZSAZi', 
+      },
+      { 
+        url: 'turn:18.220.202.114:4446?transport=udp',
+        username: 'meet.smash.cx:443',
+        credential: '60xNNKDk1zLZSAZi',
+      }
   ]
 };
 
